ef417ff67b0d6d8856b708d686682bd5cbebee26
小山尚希 <koyamanaoki@koyamanaokinoMacBook-Pro.local>
2025-09-29 21:39:23 +0900

    chore(api): register User in admin

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,13 @@
+# api/users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        ("Creaon", {"fields": ("role", "bio", "avatar", "links")}),
+    )
+    list_display = ("id", "username", "email", "role", "is_staff", "is_superuser")
+    list_filter = ("role", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
