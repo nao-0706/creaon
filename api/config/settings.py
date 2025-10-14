8318fd52699df9c1780dfc17d284141ee34bdffe
Naoki Koyama <naoki19880817@example.com>
2025-10-14 12:54:15 +0900

    wip: before merging post-model

--- api/config/settings.py
+++ api/config/settings.py
@@ -30,13 +30,14 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'corsheaders',
     'users',
 ]
@@ -136,8 +137,11 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),  # 開発中は30分で十分
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),     # 1週間で再ログイン
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
     # 必要になったらここに他のオプションも追加（例：ALGORITHM、ROTATE_REFRESH_TOKENS等）
 }
 
